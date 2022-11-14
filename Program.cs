1ba7a3f1b0d1785e9b3d965813dd584b74a9680c
aftabpatel10 <aftabpatel3107@gmail.com>
2022-11-14 18:24:31 +0530

    Checking Employee is present or Not

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageCalculation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation program");
+         
+            int EMP_IS_PRESENT = 1;
+            Random r = new Random();
+            int employeeinput = r.Next(0, 2);
+
+             if (EMP_IS_PRESENT == employeeinput)
+                    Console.WriteLine("Employee is Present");
+             else
+                    Console.WriteLine("Employee is Absent");
+            
+
+        }
+    }
+}
